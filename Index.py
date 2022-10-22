e8a6592a2f7d3eb1cb902876ffc577bf342db621
hasprogamer <112610428+hasprogamer@users.noreply.github.com>
2022-10-22 19:04:50 +0000

    Create Index.py

---
+++ Index.py
@@ -0,0 +1,10 @@
+from time import sleep
+a=float(input('  saisi température:'))
+sleep(1)
+if a<=0:
+      print('  cest la glace')
+elif a<=100:
+            print("  c'est de le l'eau")
+else:
+       print("  qc'est de la vapeur")
+sleep(1)
